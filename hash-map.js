a39f00180e8d78e3d904db0296a0831b1fb0fd58
João Oliveira <jaoovit@gmail.com>
2024-05-09 16:16:57 +0100

    create a set to add value in the hashmap and a get to see hashmap values

--- hash-map.js
+++ hash-map.js
@@ -1,22 +1,34 @@
 class HashMap {
   constructor() {
-    this.map = new Array();
+    this.map = {};
   }
 
   hash(key) {
     let hashCode = 0;
-
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
-
     return hashCode;
+  }
+  set(key) {
+    const index = this.hash(key);
+    this.map[index] = key;
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    return this.map[index];
   }
 }
 
 const map = new HashMap();
 
-console.log(map.hash("John"));
+map.set("John");
+map.set("Jess");
+map.set("Miguel");
+map.set("Amora");
+
+console.log(map.get("John"));
 
 console.log(map);
